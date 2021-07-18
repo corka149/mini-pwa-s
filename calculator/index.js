9b1dd77362a3cc86739003493e9a3faf32363b4f
corka149 <corka149@mailbox.org>
2021-07-18 14:32:27 +0200

    Cleaned code

--- calculator/index.js
+++ calculator/index.js
@@ -1,5 +1,51 @@
 var EXCHANGE_RATE = 7.4370;
 
+
+var convert = function (direction) {
+    exchangeRate = +localStorage.getItem('exchangeRate') || this.exchangeRate;
+
+    if ('toKrone' === direction && !!this.euroAmount) {
+        this.kroneAmount = Math.round(this.euroAmount * exchangeRate * 100) / 100;
+    } else if ('toKrone' === direction) {
+        this.kroneAmount = 0.0;
+        this.euroAmount = 0.0;
+    }
+
+    if ('toEuro' === direction && !!this.kroneAmount) {
+        this.euroAmount = Math.round(this.kroneAmount / exchangeRate * 100) / 100;
+    } else if ('toEuro' === direction) {
+        this.kroneAmount = 0.0;
+        this.euroAmount = 0.0;
+    }
+}
+
+var toggleEchangeExchangeRate = function () {
+    this.showChangeExchangeRate = !this.showChangeExchangeRate;
+}
+
+var setExchangeRate = function () {
+    if (this.tmpExchangeRate) {
+        this.exchangeRate = this.tmpExchangeRate;
+        localStorage.setItem('exchangeRate', this.exchangeRate);
+    }
+}
+
+var resetExchangeRate = function () {
+    this.exchangeRate = EXCHANGE_RATE;
+    this.tmpExchangeRate = EXCHANGE_RATE;
+    localStorage.setItem('exchangeRate', this.exchangeRate);
+}
+
+var clear = function (currency) {
+    if (currency === 'euro') {
+        this.euroAmount = '';
+    }
+
+    if (currency === 'krone') {
+        this.kroneAmount = '';
+    }
+}
+
 var calculator = new Vue({
     el: '#calculator',
     data: {
@@ -10,45 +56,10 @@ var calculator = new Vue({
         showChangeExchangeRate: false
     },
     methods: {
-        convert: function (direction) {
-            exchangeRate = +localStorage.getItem('exchangeRate') || this.exchangeRate;
-
-            if ('toKrone' === direction && !!this.euroAmount) {
-                this.kroneAmount = Math.round(this.euroAmount * exchangeRate * 100) / 100;
-            } else if ('toKrone' === direction) {
-                this.kroneAmount = 0.0;
-                this.euroAmount = 0.0;
-            }
-
-            if ('toEuro' === direction && !!this.kroneAmount) {
-                this.euroAmount = Math.round(this.kroneAmount / exchangeRate * 100) / 100;
-            } else if ('toEuro' === direction) {
-                this.kroneAmount = 0.0;
-                this.euroAmount = 0.0;
-            }
-        },
-        toggleEchangeExchangeRate: function () {
-            this.showChangeExchangeRate = !this.showChangeExchangeRate;
-        },
-        setExchangeRate: function () {
-            if (this.tmpExchangeRate) {
-                this.exchangeRate = this.tmpExchangeRate;
-                localStorage.setItem('exchangeRate', this.exchangeRate);
-            }
-        },
-        resetExchangeRate: function () {
-            this.exchangeRate = EXCHANGE_RATE;
-            this.tmpExchangeRate = EXCHANGE_RATE;
-            localStorage.setItem('exchangeRate', this.exchangeRate);
-        },
-        clear: function (currency) {
-            if (currency === 'euro') {
-                this.euroAmount = '';
-            }
-
-            if (currency === 'krone') {
-                this.kroneAmount = '';
-            }
-        }
+        convert: convert,
+        toggleEchangeExchangeRate: toggleEchangeExchangeRate,
+        setExchangeRate: setExchangeRate,
+        resetExchangeRate: resetExchangeRate,
+        clear: clear
     }
 })
