b8e96ebe51ab4725467b3039623e25b96dfd6db4
corka149 <corka149@mailbox.org>
2021-07-18 14:29:08 +0200

    Added toggle changeExhangeRate

--- calculator/index.js
+++ calculator/index.js
@@ -6,7 +6,8 @@ var calculator = new Vue({
         euroAmount: 0.0,
         kroneAmount: 0.0,
         tmpExchangeRate: +localStorage.getItem('exchangeRate') || EXCHANGE_RATE,
-        exchangeRate: +localStorage.getItem('exchangeRate') || EXCHANGE_RATE
+        exchangeRate: +localStorage.getItem('exchangeRate') || EXCHANGE_RATE,
+        showChangeExchangeRate: false
     },
     methods: {
         convert: function (direction) {
@@ -14,11 +15,20 @@ var calculator = new Vue({
 
             if ('toKrone' === direction && !!this.euroAmount) {
                 this.kroneAmount = Math.round(this.euroAmount * exchangeRate * 100) / 100;
+            } else if ('toKrone' === direction) {
+                this.kroneAmount = 0.0;
+                this.euroAmount = 0.0;
             }
 
             if ('toEuro' === direction && !!this.kroneAmount) {
                 this.euroAmount = Math.round(this.kroneAmount / exchangeRate * 100) / 100;
+            } else if ('toEuro' === direction) {
+                this.kroneAmount = 0.0;
+                this.euroAmount = 0.0;
             }
+        },
+        toggleEchangeExchangeRate: function () {
+            this.showChangeExchangeRate = !this.showChangeExchangeRate;
         },
         setExchangeRate: function () {
             if (this.tmpExchangeRate) {
@@ -30,6 +40,15 @@ var calculator = new Vue({
             this.exchangeRate = EXCHANGE_RATE;
             this.tmpExchangeRate = EXCHANGE_RATE;
             localStorage.setItem('exchangeRate', this.exchangeRate);
+        },
+        clear: function (currency) {
+            if (currency === 'euro') {
+                this.euroAmount = '';
+            }
+
+            if (currency === 'krone') {
+                this.kroneAmount = '';
+            }
         }
     }
 })
